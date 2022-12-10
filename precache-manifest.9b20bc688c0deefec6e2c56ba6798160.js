dce726712d3a24a67bf1dfd37cb747eb5ed30041
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-10 00:52:11 -0700

    Updates

---
+++ precache-manifest.9b20bc688c0deefec6e2c56ba6798160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4fc3ba4ae1300f999d276a8bf1d027",
+    "url": "/pagination-react/index.html"
+  },
+  {
+    "revision": "f7e5c2ff18f79e5b9706",
+    "url": "/pagination-react/static/css/main.8ca79aa1.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/pagination-react/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pagination-react/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e5c2ff18f79e5b9706",
+    "url": "/pagination-react/static/js/main.408c41de.chunk.js"
+  },
+  {
+    "revision": "7ea03c9393d9e172d860",
+    "url": "/pagination-react/static/js/runtime-main.18b85c40.js"
+  }
+]);
